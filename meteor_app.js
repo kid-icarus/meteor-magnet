0875f06a8167d539c422fd2b35362b9bb7ccbfce
Ryan Kois <ryan.kois@gmail.com>
2013-06-26 23:26:51 -0600

    Inichule.

---
+++ meteor_app.js
@@ -0,0 +1,47 @@
+Words = new Meteor.Collection("words");
+
+if (Meteor.isClient) {
+  
+  Template.hello.greeting = function () {
+    return "Words";
+  };
+
+  Template.hello.words = function () {
+    return Words.find();
+  };
+  $('.word').draggable();
+
+  Template.hello.events({
+    'click .delete' : function () {
+      // template data, if any, is available in 'this'
+      Words.remove(this._id);
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    },
+    'mouseover .word': function() {
+      $('#' + this._id).draggable();
+      console.log(this._id);
+      // $(this._id).draggable();
+    },
+
+    'keyup #words' : function(event) {
+        console.log(event); 
+        if (event.keyCode == 13) {
+          insertWord = {
+            'name': $('#words').val(),
+            'x' : 0,
+            'y' : 0
+          };
+          Words.insert(insertWord);
+          console.log("You changed words the button");
+
+        }
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
